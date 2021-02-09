67c89cdcc8f0719a68e04075dab6a86369323bb8
Taylor Bustamante <taylorbustamante16@gmail.com>
2021-02-09 15:57:08 -0800

    Done.

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -1,10 +1,22 @@
-import React from 'react';
-import { actors } from '../data';
+import React from "react";
+import { actors } from "../data";
 
 const Actors = () => {
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Actors Page</h1>
+      {actors.map((actor) => {
+        return (
+          <div>
+            <h4>{actor.name}</h4>
+            <ul>
+              {actor.movies.map((movie) => {
+                return <li>{movie}</li>;
+              })}
+            </ul>
+          </div>
+        );
+      })}
     </div>
   );
 };
--- src/components/Directors.js
+++ src/components/Directors.js
@@ -1,12 +1,25 @@
-import React from 'react';
-import { directors } from '../data';
+import React from "react";
+import { directors } from "../data";
 
 const Directors = () => {
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Directors Page</h1>
+      {directors.map((director) => {
+        return (
+          <div>
+            <h4>{director.name}</h4>
+            <ul>
+              <strong>Movies:</strong>
+              {director.movies.map((movie) => {
+                return <li>{movie}</li>;
+              })}
+            </ul>
+          </div>
+        );
+      })}
     </div>
   );
-}
+};
 
-export default Directors
+export default Directors;
